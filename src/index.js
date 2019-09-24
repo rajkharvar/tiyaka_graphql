8aa90f8de856c3343731d7dafa8fad5f5f5ebe97
Anshu <anshu@licious.com>
2019-09-24 11:28:51 +0530

    express added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('hello world')
+import express from 'express'
+
+const app = express()
+
+app.use('/', (req, res) => res.send('hello world'))
+const port = process.env.PORT || 3000
+app.listen(3000, () => console.log(`Magic Happens at port ${port}`))
